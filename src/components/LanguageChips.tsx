d73fabdd390acf72941dc49b45977c19aa9bb8f3
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-06-21 18:29:14 +0700

    Element within an Element

--- src/components/LanguageChips.tsx
+++ src/components/LanguageChips.tsx
@@ -8,13 +8,15 @@ type LanguageChipsProps = {
 }
 
 export default function LanguageChips({ languages, wrongGuessCount }: LanguageChipsProps): JSX.Element {
-    const languageElements = languages.map((lang, index) => {
-        const isLanguageLost = index < wrongGuessCount
-        const styles = {
+    const languageElements: JSX.Element[] = languages.map((lang: Language, index: number): JSX.Element => {
+
+        const isLanguageLost: boolean = index < wrongGuessCount
+        const styles: Omit<Language, "name"> = {
             backgroundColor: lang.backgroundColor,
             color: lang.color
         }
-        const className = clsx("chip", isLanguageLost && "lost")
+        const className: string = clsx("chip", isLanguageLost && "lost")
+
         return (
             <span
                 className={className}

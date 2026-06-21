1287eabee39b88ea3ddee69471b420cd45be1b61
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-06-21 11:01:25 +0700

    Typing React Components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
-export default function Header() {
+import type { JSX } from 'react'
+
+export default function Header(): JSX.Element {
     return (
         <header>
             <h1>Assembly: Endgame</h1>
@@ -7,3 +9,20 @@ export default function Header() {
         </header>
     )
 }
+
+/*
+TYPING USING REACT.FC
+
+const Header: React.FC<props> = () => {
+    return (
+        <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+        </header>
+    );
+};
+
+export default Header;
+
+*/
